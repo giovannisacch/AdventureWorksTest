c6231b9b331aaf7abbe6339f71fcbe41af75a1a3
sajoratosg <geovani.succi@sage.com>
2019-04-16 14:35:19 -0300

    projeto adicionado

---
+++ src/AdventureWorks.Api/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+    
+        // GET api/Hello
+        [HttpGet]
+        public ActionResult<string> Get(int id)
+        {
+            return "Hello from IOB Sage Adventure Works!";
+        }
+    }
+}
